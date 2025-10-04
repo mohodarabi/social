e1950d018accf0a20aa7f7faf675a5030333a0e0
mohodarabi <mohodara@gmail.com>
2025-10-04 11:09:21 +0330

    add user controller

--- internal/db/db.go
+++ internal/db/db.go
@@ -20,11 +20,17 @@ type DbRepo struct {
 		Create(context.Context, *UserModel) error
 		GetById(context.Context, int64) (*UserModel, error)
 	}
+
+	Comments interface {
+		Create(context.Context, *CommentModel) error
+		GetById(context.Context, int64) (*CommentModel, error)
+	}
 }
 
 func PostgresDb(connection *sql.DB) DbRepo {
 	return DbRepo{
-		Posts: &PostRepo{connection},
-		Users: &UserRepo{connection},
+		Posts:    &PostRepo{connection},
+		Users:    &UserRepo{connection},
+		Comments: &CommentRepo{connection},
 	}
 }
