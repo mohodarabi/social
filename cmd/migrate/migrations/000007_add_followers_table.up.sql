633a6cd283679dc82700c0689f321ca2dbbaccdf
mohodarabi <mohodara@gmail.com>
2025-11-08 12:21:13 +0330

    add followers table and migration

--- cmd/migrate/migrations/000007_add_followers_table.up.sql
+++ cmd/migrate/migrations/000007_add_followers_table.up.sql
@@ -1,9 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE IF NOT EXISTS followers (
-    id bigserial PRIMARY KEY,
-    user_id bigint NOT NULL,
-    follower_id bigint NOT NULL,
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+user_id bigint NOT NULL,
+follower_id bigint NOT NULL,
+created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+PRIMARY KEY (user_id, follower_id),
+FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+FOREIGN KEY (follower_id) REFERENCES users (id) ON DELETE CASCADE
 );
