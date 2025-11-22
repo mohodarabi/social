4d38eca267f8bb98fa738185109c682362adc81a
moho <mohodarabi@gmail.com>
2025-11-22 11:15:50 +0330

    add indexes in database

---
+++ cmd/migrate/migrations/000008_add_index.down.sql
@@ -0,0 +1,8 @@
+
+DROP INDEX EXISTS idx_comments_content;
+DROP INDEX EXISTS idx_posts_title;
+DROP INDEX EXISTS idx_posts_tags;
+DROP INDEX EXISTS idx_users_username;
+DROP INDEX EXISTS idx_users_user_id;
+DROP INDEX EXISTS idx_comments_post_id;
+ 
