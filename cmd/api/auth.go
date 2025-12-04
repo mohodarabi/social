f569b214721a74cf2e92dd6b4c6dc4c413615b79
moho <mohodarabi@gmail.com>
2025-12-04 18:29:59 +0330

    add create user and invitation

---
+++ cmd/api/auth.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
+	"social/internal/db"
+
+	"github.com/google/uuid"
+)
+
+type UserAuthenticate struct {
+	Username string `json:"username" validate:"required,max=100"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,max=20"`
+}
+
+// CreateUser godoc
+//
+//	@Summary		create user and invitation
+//	@Description	create user and invitation
+//	@Tags			users
+//	@Accept			json
+//	@Produce		json
+//	@Param			payload	body		UserAuthenticate	true	"User authentication payload"
+//	@Success		201		{object}	map[string]interface{}
+//	@Failure		400		{object}	error
+//	@Failure		404		{object}	error
+//	@Failure		500		{object}	error
+//	@Router			/auth/user [post]
+func (app *application) authHandler(response http.ResponseWriter, request *http.Request) {
+	var payload UserAuthenticate
+
+	if err := readJson(response, request, &payload); err != nil {
+		app.badRequestError(response, request, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestError(response, request, err)
+		return
+	}
+
+	ctx := request.Context()
+
+	user := &db.UserModel{
+		Username: payload.Username,
+		Email:    payload.Email,
+	}
+
+	if err := user.Password.Set(payload.Password); err != nil {
+		app.internalServerError(response, request, err)
+		return
+	}
+
+	plainToken := uuid.New().String()
+	hash := sha256.Sum256([]byte(plainToken))
+	hashToken := hex.EncodeToString(hash[:])
+
+	err := app.db.Users.CreateAndInvite(ctx, user, hashToken, app.config.mail.expTime)
+
+	if err != nil {
+		switch err {
+		case db.ErrDuplicateEmail:
+			app.badRequestError(response, request, err)
+			return
+		case db.ErrDuplicateUsername:
+			app.badRequestError(response, request, err)
+			return
+		default:
+			app.internalServerError(response, request, err)
+			return
+		}
+	}
+
+	responseDate := map[string]string{
+		"message": "successfully created",
+	}
+
+	if err := writeJson(response, http.StatusOK, responseDate); err != nil {
+		app.internalServerError(response, request, err)
+		return
+	}
+
+}
